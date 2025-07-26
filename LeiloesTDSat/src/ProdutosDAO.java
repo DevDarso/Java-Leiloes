2e1eaf2f6537ae052feb7d5083626a1cc85b9431
Darso <thiagodarso2025@gmail.com>
2025-07-26 15:38:24 -0300

    Mensagem de sucesso ao cadastrar

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -1,17 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
@@ -22,21 +13,129 @@ public class ProdutosDAO {
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
-    public void cadastrarProduto (ProdutosDTO produto){
+    public boolean cadastrarProduto (ProdutosDTO produto) throws ClassNotFoundException{
         
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+        String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?,?,?)";
+        conectaDAO conn = null;
+        PreparedStatement ps = null;
+
+        try {
+            conn = new conectaDAO();
+            conn.conectar();
+            ps = conn.getConexao().prepareStatement(sql);
+
+            ps.setString(1, produto.getNome());
+            ps.setDouble(2, produto.getValor());
+            ps.setString(3, produto.getStatus());
+           
+            int rowsAffected = ps.executeUpdate();
+            if (rowsAffected > 0) {
+               // System.out.println("Produto adicionado com sucesso!");
+//                JOptionPane.showMessageDialog(null, "Produto adicionado com sucesso!");
+                return true;
+            }
+        } catch (SQLException ex) {
+            System.err.println("Erro ao adicionar no banco de dados: " + ex.getMessage());
+        } finally {
+            try {
+                if (ps != null) ps.close();
+                if (conn != null) conn.desconectar();
+            } catch (SQLException e) {
+                System.err.println("Erro ao fechar recursos: " + e.getMessage());
+            }
+        }
+        return false;
     }
-    
+ 
     public ArrayList<ProdutosDTO> listarProdutos(){
-        
+        ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+        conectaDAO conexao = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+            conexao = new conectaDAO();
+            conexao.conectar();
+            String sql = "SELECT * FROM produtos";
+            ps = conexao.getConexao().prepareStatement(sql);
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                
+                produto.setId(rs.getInt("id"));
+                produto.setNome(rs.getString("nome"));
+                produto.setValor(rs.getInt("valor"));
+                produto.setStatus(rs.getString("status"));
+                listagem.add(produto);
+            }
+        } catch (SQLException se) {
+            System.err.println("Erro ao listar serviços: " + se.getMessage());
+        } finally {
+            try {
+                if (rs != null) rs.close();
+                if (ps != null) ps.close();
+                if (conexao != null) conexao.desconectar();
+            } catch (SQLException e) {
+                System.err.println("Erro ao fechar recursos: " + e.getMessage());
+            }
+        }
         return listagem;
+    }    
+       public static boolean atualizarStatus(int id) {
+    conectaDAO conexao = null;
+    PreparedStatement ps = null;
+
+    try {
+        conexao = new conectaDAO();
+        conexao.conectar();
+
+        String sql = "UPDATE produtos SET status = 'Vendido' " +
+             "WHERE id = ? ";
+
+        ps = conexao.getConexao().prepareStatement(sql);
+        ps.setInt(1, id);
+
+        return ps.executeUpdate() > 0;
+    } catch (SQLException e) {
+        System.err.println("Erro ao atualizar status do produto: " + e.getMessage());
+        return false;
     }
-    
-    
-    
-        
 }
+       public ArrayList<ProdutosDTO> listarVendidos(){
+        ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+        conectaDAO conexao = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+            conexao = new conectaDAO();
+            conexao.conectar();
+            String sql = "SELECT * FROM produtos WHERE status = 'Vendido'";
+            ps = conexao.getConexao().prepareStatement(sql);
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                
+                produto.setId(rs.getInt("id"));
+                produto.setNome(rs.getString("nome"));
+                produto.setValor(rs.getInt("valor"));
+                produto.setStatus(rs.getString("status"));
+                listagem.add(produto);
+            }
+        } catch (SQLException se) {
+            System.err.println("Erro ao listar serviços: " + se.getMessage());
+        } finally {
+            try {
+                if (rs != null) rs.close();
+                if (ps != null) ps.close();
+                if (conexao != null) conexao.desconectar();
+            } catch (SQLException e) {
+                System.err.println("Erro ao fechar recursos: " + e.getMessage());
+            }
+        }
+        return listagem;
+    }   
+ }
 
--- LeiloesTDSat/src/conectaDAO.java
+++ LeiloesTDSat/src/conectaDAO.java
@@ -1,33 +1,52 @@
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
 public class conectaDAO {
-    
-    public Connection connectDB(){
-        Connection conn = null;
-        
-        try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
-            
-        } catch (SQLException erro){
-            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
-        }
-        return conn;
+    private Connection conexao;
+
+    // Construtor para inicializar a conexão
+    public conectaDAO(Connection conexao) {
+        this.conexao = conexao;
     }
-    
+
+    // Construtor sem parâmetros
+    public conectaDAO() {
+    }
+
+    // Método para obter a conexão
+    public Connection getConexao() {
+        return conexao;
+    }
+
+    // Método para configurar a conexão
+    public void setConexao(Connection conexao) {
+        this.conexao = conexao;
+    }
+
+    // Método para conectar ao banco de dados
+    public void conectar() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexao = DriverManager.getConnection("jdbc:mysql://localhost:3306/uc11?user=root&password=Devdarso2025");
+            System.out.println("Sucesso na conexão");
+        } catch (ClassNotFoundException cn) {
+            System.out.println("Falha ao conectar com banco" + cn);
+        } catch (SQLException sql) {
+            System.out.println("Erro de SQL" + sql);
+        }
+    }
+
+    // Método para desconectar do banco de dados
+    public void desconectar() {
+        try {
+            if (conexao != null) {
+                conexao.close();
+                System.out.println("Desconectado");
+            }
+        } catch (SQLException sql) {
+            System.out.println("Erro ao desconectar do banco de dados" + sql);
+        }
+    }
 }
